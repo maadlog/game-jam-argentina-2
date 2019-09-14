0e61de7156315296b0178aa72b8b4cf041b397b2
Sefirost <fabriciocarella@hotmail.com>
2019-09-14 18:53:49 -0300

    movement player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+    public float rotateSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, 0.0f, -Input.GetAxis("Horizontal") * rotateSpeed);
+        float moveVertical = Input.GetAxis("Vertical");
+        transform.position += moveVertical * transform.right * movementSpeed * Time.deltaTime;
+    }
+}
