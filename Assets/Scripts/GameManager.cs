064e7ad66031ad41eb98dada5cdee630e52995a2
Sefirost <fabriciocarella@hotmail.com>
2019-09-14 20:07:20 -0300

    add game manager and turret

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+	public float timeToMoveToMenu = 2f;
+	public Text scoreText;
+
+	int score = 0;
+	GameManager gameManager;
+	float menuTimer;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+        // set time to timer
+        menuTimer = timeToMoveToMenu;
+
+        // make it singleton
+		if (gameManager == null)
+		{
+			gameManager = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void UpdateScore(int score)
+	{
+		this.score += score;
+		scoreText.text = this.score.ToString();
+	}
+
+	public void ChangeLevel()
+	{
+		SceneManager.LoadScene("Level2");
+	}
+
+	public void LostLevel()
+	{
+		// player cant move
+        
+		// go to menu after a while
+        if(menuTimer < 0) {
+            SceneManager.LoadScene("Menu");
+        } else {
+            menuTimer -= Time.deltaTime;
+        }
+	}
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+	Transform target;
+    float timerToFindNext;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (target != null)
+		{
+			// Shoot
+		}
+		else
+		{
+			if (timerToFindNext < 0)
+			{
+
+			}
+			else
+			{
+
+			}
+		}
+	}
+
+	void FindNearestEnemy()
+	{
+		float enemyMinDistance = float.MaxValue;
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+		for (int i = 0; i < enemies.Length; i++)
+		{
+			float enemyDisctance = Vector2.Distance(transform.position, enemies[i].transform.position);
+			if (enemyDisctance < enemyMinDistance)
+			{
+				target = enemies[i].transform;
+				enemyMinDistance = enemyDisctance;
+			}
+		}
+	}
+}
