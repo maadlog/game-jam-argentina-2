0608e4ab6615336dc215b63393732313c9d7086e
Frankie239 <55329447+Frankie239@users.noreply.github.com>
2019-09-15 12:20:23 -0300

    animacion
    Animacion de las slides

---
+++ Assets/Scenes/fade.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class fade : MonoBehaviour
+{
+    public GameObject button; //objeto boton al que apunta
+    public typing tecleo; //Objeto de la clase typewriter
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    private bool flag;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        flag = tecleo.finish;
+        
+            
+        button.SetActive(flag); //La opacidad del boton es el maximo
+     
+    }
+}
