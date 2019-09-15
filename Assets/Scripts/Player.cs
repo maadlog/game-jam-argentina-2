87710a15d034ec6eb9b785b2bd120d7c76c703b2
Sefirost <fabriciocarella@hotmail.com>
2019-09-15 10:27:04 -0300

    changed sprites

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,35 +9,55 @@ public class Player : MonoBehaviour
 	public float rotateSpeed = 1f;
 	public float shootColdDown = 2f;
 
-    public Transform[] guns;
+	public Transform[] guns;
 	public GameObject gunLeft;
 	public GameObject bullet;
 
 	float shootTimer;
+	Animator cameraAnimator;
 	Animator animator;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		animator = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+		cameraAnimator = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+		animator = GetComponent<Animator>();
 		shootTimer = shootColdDown;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-        Movement();
+		Movement();
 
 		Shoot();
 	}
 
-    void Movement() {
-        // Handle rotation
-		transform.Rotate(0.0f, 0.0f, -Input.GetAxis("Horizontal") * rotateSpeed);
+	void Movement()
+	{
+		// get input
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
 
-        // Handle foward or backwards
-		float moveVertical = Input.GetAxis("Vertical");
-		transform.position += moveVertical * transform.right * movementSpeed * Time.deltaTime;
-    }
+		if (horizontal != 0 || vertical != 0)
+		{
+			// set move boolean to state moving
+			animator.SetBool("isMoving", true);
+
+			// handle rotation
+			transform.Rotate(0.0f, 0.0f, -horizontal * rotateSpeed);
+
+			// handle foward or backwards
+			float moveVertical = vertical;
+			transform.position += moveVertical * transform.right * movementSpeed * Time.deltaTime;
+		}
+		else
+		{
+			// set move boolean to state idle
+			animator.SetBool("isMoving", false);
+		}
+
+	}
 
 	float timeShooting = 0f;
 	float timeShootingLimit = 1f;
@@ -62,19 +82,19 @@ public class Player : MonoBehaviour
 
 				if (proportion > 0.2)
 				{
-					animator.Play("Shoot");
+					cameraAnimator.Play("Shoot");
 				}
 
-                foreach (Transform gun in guns)
-                {
-                    var displace = gun.TransformVector(new Vector3(0, disp, 0));
-				    var trasn = gun.position + displace;
-				    Instantiate(bullet, trasn, transform.rotation);
-                }
+				foreach (Transform gun in guns)
+				{
+					var displace = gun.TransformVector(new Vector3(0, disp, 0));
+					var trasn = gun.position + displace;
+					Instantiate(bullet, trasn, transform.rotation);
+				}
 
 				// var displace = gunLeft.transform.TransformVector(new Vector3(0, disp, 0));
 				// var trasn = gunLeft.transform.position + displace;
-                // Instantiate(bullet, trasn, transform.rotation);
+				// Instantiate(bullet, trasn, transform.rotation);
 				shootTimer = shootColdDown;
 			}
 			else
