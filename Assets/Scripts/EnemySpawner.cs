1d4fa907b065883670d4df472aec418e9645ea64
Martín Loguancio <martin.loguancio@gmail.com>
2019-09-20 18:51:27 -0300

    Added Portal

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,17 +1,19 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
+
 
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject prefab;
     public GameObject spawnPoint;
 
-    float enemyRate = 5; // 1 Enemies generated in 5 secs
-    float enemyDelay = 1; // Time delay to generate enemy
+    float enemyRate = 2; // 1 Enemies generated every 2 secs
+    float enemyDelay = 2; // Time delay to generate enemy
 
-    float enemyRateDecrease = 0.9f; // How much the rate decreases with each generation
-    float minimunEnemyRate = 1; //Minimun rate for enemy spawnage
+    bool simultaneousSpawn;
+
+    float enemyRateDecrease = 0.8f; // How much the rate decreases with each generation
+    float minimunEnemyRate = 0.6f; //Minimun rate for enemy spawnage
 
     // Start is called before the first frame update
     void Start()
@@ -19,46 +21,81 @@ public class EnemySpawner : MonoBehaviour
         
     }
 
+    int spawnedInThisGeneration;
+    int deltaNextGeneration = 10;
+
     // Update is called once per frame
     void Update()
     {
         enemyDelay -= Time.deltaTime;
-
+        
         if (enemyDelay <= 0)
         {
-            //Time to instantiate an enemy
+            ResetTimers();
 
-            //Reset timers
-            enemyDelay = enemyRate;
-            //Decrease rate
-            enemyRate *= enemyRateDecrease;
-            if (enemyRate < minimunEnemyRate)
+            SpawnEnemy();
+
+            if (spawnedInThisGeneration >= deltaNextGeneration)
             {
-                enemyRate = minimunEnemyRate;
+                ResetGenerationCounters();
+                MoveSpawnPoint();
             }
-
-            //Generate random point in sphere centered on this Spawner
-            Vector3 offset = Random.onUnitSphere;
-            offset.z = 0;
-            offset = offset.normalized * spawnDistance(); // Normalize direction and get distance
-
-            GameObject newEnemy = Instantiate(
-                prefab //Enemy prefab
-                , spawnPoint.transform.position + offset //Initial Position
-                , Quaternion.identity); // Dont Rotate
-
-            EnemyMovement newEnemyMov;
-
-            if (newEnemy.TryGetComponent<EnemyMovement>(out newEnemyMov))
+            else
             {
-                newEnemyMov.VarySpeedRandom();
+                spawnedInThisGeneration++;
             }
-            
         }
     }
 
-    float spawnDistance()
+    private void MoveSpawnPoint()
     {
-        return 12 * Random.Range(0.8f, 1.2f);
+        //Generate random point in sphere centered on this Spawner
+        Vector3 newPosition = Random.onUnitSphere;
+        newPosition.z = 0;
+        newPosition = newPosition.normalized * spawnPoint.transform.position.magnitude; // Normalize direction and get distance
+
+        spawnPoint.transform.position = newPosition;
     }
+
+    private void ResetGenerationCounters()
+    {
+        spawnedInThisGeneration = 0;
+        deltaNextGeneration = Random.Range(8, 12);
+    }
+
+    private void SpawnEnemy()
+    {
+        float offsetRotation;
+        if (spawnPoint.transform.position.x > 0)
+        { 
+            offsetRotation = Random.Range(0f, 180f);
+        } else
+        {
+            offsetRotation = Random.Range(0f, -180f);
+        }
+        
+
+        Instantiate(
+            prefab //Enemy prefab
+            , spawnPoint.transform.position
+            , Quaternion.Euler(0,0, offsetRotation));
+        
+
+        
+       
+    }
+
+    private void ResetTimers()
+    {
+        //Reset timers
+        enemyDelay = enemyRate;
+        //Decrease rate
+        enemyRate *= enemyRateDecrease;
+        if (enemyRate < minimunEnemyRate)
+        {
+            enemyRate = minimunEnemyRate;
+        }
+    }
+
+
 }
