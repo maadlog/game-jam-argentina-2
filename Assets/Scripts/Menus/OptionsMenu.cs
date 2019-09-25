c2aad5df1b339ea4f3c7cecf878de70188c9ee85
Sefirost <fabriciocarella@hotmail.com>
2019-09-25 01:05:26 -0300

    update ui in level 1

---
+++ Assets/Scripts/Menus/OptionsMenu.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour
+{
+	public enum Axis // your custom enumeration
+	{
+		Horizontal,
+		Vertical
+	};
+
+	public bool isActivated = false;
+	public float selectedAnimationSpeed = 0.5f;
+	public Axis movementAxis = Axis.Vertical;
+	public Button[] options = default;
+
+	int indexOption = 0;
+	float animationTimer = 0f;
+	float initialPercentage = 1f;
+	float scaleFactor = 1.1f;
+
+	float timerAxis = 0;
+	float axisColdDownTime = 0.1f;
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (isActivated)
+		{
+			SelectedAnimation();
+			CheckChangeSelectedOption();
+			CheckEnterOption();
+		}
+
+	}
+
+	void SelectedAnimation()
+	{
+		if (animationTimer < selectedAnimationSpeed)
+		{
+			float slope = (scaleFactor - initialPercentage) / selectedAnimationSpeed;
+			float increaseFactor = animationTimer * slope + initialPercentage;
+			options[indexOption].transform.localScale = new Vector3(increaseFactor, increaseFactor, 1);
+			animationTimer += Time.unscaledDeltaTime;
+		}
+		else if (animationTimer > selectedAnimationSpeed && animationTimer < selectedAnimationSpeed * 2)
+		{
+			float slope = (initialPercentage - scaleFactor) / (selectedAnimationSpeed * 2 - selectedAnimationSpeed);
+			float increaseFactor = animationTimer * slope + (initialPercentage - slope * selectedAnimationSpeed * 2);
+			options[indexOption].transform.localScale = new Vector3(increaseFactor, increaseFactor, 1);
+			animationTimer += Time.unscaledDeltaTime;
+		}
+		else
+		{
+			animationTimer = 0f;
+		}
+	}
+
+	void CheckChangeSelectedOption()
+	{
+		if (timerAxis < 0f)
+		{
+			if (Input.GetAxisRaw(movementAxis.ToString()) == 1)
+			{
+				options[indexOption].transform.localScale = new Vector3(1, 1, 1);
+				indexOption++;
+			}
+			else if (Input.GetAxisRaw(movementAxis.ToString()) == -1)
+			{
+				options[indexOption].transform.localScale = new Vector3(1, 1, 1);
+				indexOption--;
+			}
+
+			if (indexOption >= options.Length)
+			{
+				indexOption = 0;
+			}
+			if (indexOption < 0)
+			{
+				indexOption = options.Length - 1;
+			}
+			timerAxis = axisColdDownTime;
+		}
+		else
+		{
+			timerAxis -= Time.unscaledDeltaTime;
+		}
+	}
+
+	void CheckEnterOption()
+	{
+		if (Input.GetButton("Submit"))
+		{
+			options[indexOption].onClick.Invoke();
+		}
+	}
+}
--- Assets/Scripts/Menus/PauseMenu.cs
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -2,25 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 public class PauseMenu : MonoBehaviour
 {
 	public GameObject pauseMenu;
-	public Button[] options = default;
-	public float selectedAnimationSpeed = 1f;
 
 	bool isPaused = false;
-	int indexOption = 0;
-	float animationTimer = 0f;
-	float initialPercentage = 1f;
-	float scaleFactor = 1.1f;
+	OptionsMenu optionsMenu;
 
-	float timerAxis = 0;
-	float axisColdDownTime = 0.1f;
-
+	void Start() {
+		optionsMenu = GetComponent<OptionsMenu>();
+	}
+	
     // Update is called once per frame
-   
     void Update()
 	{
 		if (Input.GetButtonDown("Cancel"))
@@ -34,75 +28,6 @@ public class PauseMenu : MonoBehaviour
 				Pause();
 			}
 		}
-
-		if (isPaused)
-		{
-			SelectedAnimation();
-			CheckChangeSelectedOption();
-			CheckEnterOption();
-		}
-
-	}
-
-	void SelectedAnimation()
-	{
-		if (animationTimer < selectedAnimationSpeed)
-		{
-			float slope = (scaleFactor - initialPercentage) / selectedAnimationSpeed;
-			float increaseFactor = animationTimer * slope + initialPercentage;
-			options[indexOption].transform.localScale = new Vector3(increaseFactor, increaseFactor, 1);
-			animationTimer += Time.unscaledDeltaTime;
-		}
-		else if (animationTimer > selectedAnimationSpeed && animationTimer < selectedAnimationSpeed * 2)
-		{
-			float slope = (initialPercentage - scaleFactor) / (selectedAnimationSpeed * 2 - selectedAnimationSpeed);
-			float increaseFactor = animationTimer * slope + (initialPercentage - slope * selectedAnimationSpeed * 2);
-			options[indexOption].transform.localScale = new Vector3(increaseFactor, increaseFactor, 1);
-			animationTimer += Time.unscaledDeltaTime;
-		}
-		else
-		{
-			animationTimer = 0f;
-		}
-	}
-
-	void CheckChangeSelectedOption()
-	{
-		if (timerAxis < 0f)
-		{
-			if (Input.GetAxisRaw("Vertical") == 1)
-			{
-				options[indexOption].transform.localScale = new Vector3(1, 1, 1);
-				indexOption++;
-			}
-			else if (Input.GetAxisRaw("Vertical") == -1)
-			{
-				options[indexOption].transform.localScale = new Vector3(1, 1, 1);
-				indexOption--;
-			}
-
-			if (indexOption >= options.Length)
-			{
-				indexOption = 0;
-			}
-			if (indexOption < 0)
-			{
-				indexOption = options.Length - 1;
-			}
-			timerAxis = axisColdDownTime;
-		}
-		else
-		{
-			timerAxis -= Time.unscaledDeltaTime;
-		}
-	}
-
-	void CheckEnterOption()
-	{
-		if (Input.GetButton("Submit"))
-		{
-			options[indexOption].onClick.Invoke();
-		}
 	}
 
 	void Pause()
@@ -110,6 +35,10 @@ public class PauseMenu : MonoBehaviour
 		pauseMenu.SetActive(true);
 		Time.timeScale = 0f;
         isPaused = true;
+
+		if(optionsMenu) {
+			optionsMenu.isActivated = true;
+		}
 	}
 
 	public void Continue()
@@ -117,13 +46,15 @@ public class PauseMenu : MonoBehaviour
 		pauseMenu.SetActive(false);
 		Time.timeScale = 1f;
         isPaused = false;
+
+		if(optionsMenu) {
+			optionsMenu.isActivated = false;
+		}
 	}
 
-	public void Quit()
+	public void Menu()
 	{
 		Time.timeScale = 1f;
 		SceneManager.LoadScene(0);
 	}
-
-    
 }
