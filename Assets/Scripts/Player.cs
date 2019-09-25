a550383493fe5c3adb7b7414f2742fe203180355
Sefirost <fabriciocarella@hotmail.com>
2019-09-24 23:17:39 -0300

    fix format on scripts

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,184 +6,178 @@ using System.Linq;
 
 public class Player : MonoBehaviour
 {
-    public float movementSpeed = 1f;
-    public float rotateSpeed = 1f;
-    public float shootColdDown = 2f;
+	public float movementSpeed = 1f;
+	public float rotateSpeed = 1f;
+	public float shootColdDown = 2f;
 
-    public float allowedDistanceFromBase = 35f;
+	public float allowedDistanceFromBase = 35f;
 
-    public GameObject bullet;
+	public GameObject bullet;
 
-    float shootTimer;
-    Animator cameraAnimator;
-    Animator animator;
+	float shootTimer;
+	Animator cameraAnimator;
+	Animator animator;
 
-    System.Random random = new System.Random();
-    public AudioSource[] ShootSounds;
+	System.Random random = new System.Random();
+	public AudioSource[] ShootSounds;
 
-    private Indicator gunIndicator { get; set; }
+	private Indicator gunIndicator { get; set; }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //  ShootSound.GetComponent<AudioSource>();
-        cameraAnimator = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
-        animator = GetComponent<Animator>();
-        shootTimer = shootColdDown;
-        gunIndicator = GameObject
-            .FindGameObjectsWithTag("GunIndicator")
-            .FirstOrDefault(x => x.name.Contains("MainGun"))
-            ?.GetComponent<Indicator>();
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		//  ShootSound.GetComponent<AudioSource>();
+		cameraAnimator = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+		animator = GetComponent<Animator>();
+		shootTimer = shootColdDown;
+		gunIndicator = GameObject
+			.FindGameObjectsWithTag("GunIndicator")
+			.FirstOrDefault(x => x.name.Contains("MainGun"))
+			?.GetComponent<Indicator>();
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        Movement();
+	// Update is called once per frame
+	void Update()
+	{
+		Movement();
 
-        Shoot();
-    }
+		Shoot();
+	}
 
-    void Movement()
-    {
-        // get input
-       
-            float horizontal = Input.GetAxis("Horizontal");
-            float vertical = Input.GetAxis("Vertical");
+	void Movement()
+	{
+		// get input
 
-            if (horizontal != 0 || vertical != 0)
-            {
-                // set move boolean to state moving
-                animator.SetBool("isMoving", true);
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
 
-                // handle rotation
-                transform.Rotate(0.0f, 0.0f, -horizontal * rotateSpeed);
+		if (horizontal != 0 || vertical != 0)
+		{
+			// set move boolean to state moving
+			animator.SetBool("isMoving", true);
 
-                // handle foward or backwards
-                float moveVertical = vertical;
-                transform.position += moveVertical * transform.right * movementSpeed * Time.deltaTime;
-                transform.position = new Vector3(Mathf.Clamp(transform.position.x, -allowedDistanceFromBase, allowedDistanceFromBase), Mathf.Clamp(transform.position.y, -allowedDistanceFromBase, allowedDistanceFromBase));
-            }
-            else
-            {
-                // set move boolean to state idle
-                animator.SetBool("isMoving", false);
-            }
-        
-        
+			// handle rotation
+			transform.Rotate(0.0f, 0.0f, -horizontal * rotateSpeed);
 
-    }
+			// handle foward or backwards
+			float moveVertical = vertical;
+			transform.position += moveVertical * transform.right * movementSpeed * Time.deltaTime;
+			transform.position = new Vector3(Mathf.Clamp(transform.position.x, -allowedDistanceFromBase, allowedDistanceFromBase), Mathf.Clamp(transform.position.y, -allowedDistanceFromBase, allowedDistanceFromBase));
+		}
+		else
+		{
+			// set move boolean to state idle
+			animator.SetBool("isMoving", false);
+		}
+	}
 
-    bool isStaggered = false;
-    float weaponHeatLimit = 4f;
-    float weaponHeat = 0f;
-    float weaponCoolOffRate = 0.5f;
+	bool isStaggered = false;
+	float weaponHeatLimit = 4f;
+	float weaponHeat = 0f;
+	float weaponCoolOffRate = 0.5f;
 
-    private bool Shooting()
-    {
-        return Input.GetKey(KeyCode.Space) || Input.GetAxisRaw("Fire1") == 1;
-    }
-    private bool Fixing()
-    {
-        return Input.GetKey(KeyCode.Q)
-            || Input.GetKey(KeyCode.E)
-            || Input.GetButtonDown("FixA")   //Map to koystick L & R
-            || Input.GetButtonDown("FixB");
-    }
+	private bool Shooting()
+	{
+		return Input.GetKey(KeyCode.Space) || Input.GetAxisRaw("Fire1") == 1;
+	}
+	private bool Fixing()
+	{
+		return Input.GetKey(KeyCode.Q)
+			|| Input.GetKey(KeyCode.E)
+			|| Input.GetButtonDown("FixA")   //Map to koystick L & R
+			|| Input.GetButtonDown("FixB");
+	}
 
-    private void FixedUpdate()
-    {
-        if (isStaggered)
-        {
-            if (Shooting())
-            {
-                return;
-            }
-            if (Fixing())
-            {
-                weaponHeat -= Time.deltaTime * (2 + weaponCoolOffRate);
-            }
-            else
-            {
-                weaponHeat -= Time.deltaTime * weaponCoolOffRate;
-            }
-            if (weaponHeat <= 0)
-            {
-                weaponHeat = 0;
-                isStaggered = false;
-                animator.SetBool("isStaggered", false);
-            }
+	private void FixedUpdate()
+	{
+		if (isStaggered)
+		{
+			if (Shooting())
+			{
+				return;
+			}
+			if (Fixing())
+			{
+				weaponHeat -= Time.deltaTime * (2 + weaponCoolOffRate);
+			}
+			else
+			{
+				weaponHeat -= Time.deltaTime * weaponCoolOffRate;
+			}
+			if (weaponHeat <= 0)
+			{
+				weaponHeat = 0;
+				isStaggered = false;
+				animator.SetBool("isStaggered", false);
+			}
 
-            gunIndicator?.SetRate(weaponHeat / weaponHeatLimit);
+			gunIndicator?.SetRate(weaponHeat / weaponHeatLimit);
 
-            return;
-        }
-        if (Shooting())
-        {
-            weaponHeat += Time.deltaTime;
+			return;
+		}
+		if (Shooting())
+		{
+			weaponHeat += Time.deltaTime;
 
-            var heatProportion = weaponHeat / weaponHeatLimit;
-            gunIndicator?.SetRate(heatProportion);
-            if (weaponHeat >= weaponHeatLimit)
-            {
-                isStaggered = true;
-                animator.SetBool("isStaggered", true);
-                return;
-            }
+			var heatProportion = weaponHeat / weaponHeatLimit;
+			gunIndicator?.SetRate(heatProportion);
+			if (weaponHeat >= weaponHeatLimit)
+			{
+				isStaggered = true;
+				animator.SetBool("isStaggered", true);
+				return;
+			}
 
-        }
-        else
-        {
-            if (!Shooting())
-            {
-                weaponHeat -= Time.deltaTime * weaponCoolOffRate;
-                if (weaponHeat <= 0)
-                {
-                    weaponHeat = 0;
-                }
-                gunIndicator?.SetRate(weaponHeat / weaponHeatLimit);
-            }
+		}
+		else
+		{
+			if (!Shooting())
+			{
+				weaponHeat -= Time.deltaTime * weaponCoolOffRate;
+				if (weaponHeat <= 0)
+				{
+					weaponHeat = 0;
+				}
+				gunIndicator?.SetRate(weaponHeat / weaponHeatLimit);
+			}
 
-        }
-    }
+		}
+	}
 
-    void Shoot()
-    {
-        if (isStaggered)
-        {
-            return;
-        }
-        if (shootTimer < 0 && Shooting())
-        {  
-            var heatProportion = weaponHeat / weaponHeatLimit;
-              
-            var sign = (int)Math.Round(UnityEngine.Random.Range(-1f, 1f), 0);
+	void Shoot()
+	{
+		if (isStaggered)
+		{
+			return;
+		}
+		if (shootTimer < 0 && Shooting())
+		{
+			var heatProportion = weaponHeat / weaponHeatLimit;
 
-            foreach (GameObject gun in GameObject.FindGameObjectsWithTag("Gun"))
-            {
-                var intDisplace = UnityEngine.Random.Range(0f, .1f) * sign;
-                var disp = intDisplace * heatProportion;
+			var sign = (int)Math.Round(UnityEngine.Random.Range(-1f, 1f), 0);
 
-                var displace = gun.transform.TransformVector(new Vector3(0, disp, 0));
-                var trasn = gun.transform.position + displace;
-                Instantiate(bullet, trasn, transform.rotation);
-                PlayShootSound();
-            }
+			foreach (GameObject gun in GameObject.FindGameObjectsWithTag("Gun"))
+			{
+				var intDisplace = UnityEngine.Random.Range(0f, .1f) * sign;
+				var disp = intDisplace * heatProportion;
 
-            shootTimer = shootColdDown;   
-        }
-        else
-        {
-            shootTimer -= Time.deltaTime;
-        }
-    }
+				var displace = gun.transform.TransformVector(new Vector3(0, disp, 0));
+				var trasn = gun.transform.position + displace;
+				Instantiate(bullet, trasn, transform.rotation);
+				PlayShootSound();
+			}
 
-    void PlayShootSound()
-    {
-       var i = random.Next(0, ShootSounds.Length);
-        ShootSounds[i].Play();
-    }
+			shootTimer = shootColdDown;
+		}
+		else
+		{
+			shootTimer -= Time.deltaTime;
+		}
+	}
 
-    
-
+	void PlayShootSound()
+	{
+		var i = random.Next(0, ShootSounds.Length);
+		ShootSounds[i].Play();
+	}
 }
--- Assets/Scripts/Utils/FaceTaggedObject.cs
+++ Assets/Scripts/Utils/FaceTaggedObject.cs
@@ -5,139 +5,144 @@ using UnityEngine;
 
 public interface IRotationer
 {
-    Quaternion GetRotationUpdate(float deltaTime, Transform baseTransform);
+	Quaternion GetRotationUpdate(float deltaTime, Transform baseTransform);
 }
 
-public class DampRotationer: IRotationer
+public class DampRotationer : IRotationer
 {
-    public GameObject objective;
-    public SpriteRenderer parentSprite;
+	public GameObject objective;
+	public SpriteRenderer parentSprite;
 
-    public DampRotationer(GameObject objective, SpriteRenderer parentSprite)
-    {
-        this.objective = objective;
-        this.parentSprite = parentSprite;
-    }
+	public DampRotationer(GameObject objective, SpriteRenderer parentSprite)
+	{
+		this.objective = objective;
+		this.parentSprite = parentSprite;
+	}
 
-    float quickRotationSpeed = 80;
-    float mediumRotationSpeed = 30;
-    float slowRotationSpeed = 15;
-    float speed;
+	// float quickRotationSpeed = 80;
+	float mediumRotationSpeed = 30;
+	float slowRotationSpeed = 15;
+	float speed;
 
-    public Quaternion GetRotationUpdate(float deltaTime, Transform baseTransform)
-    {
-        Vector3 dir = objective.transform.position - baseTransform.position;
-        dir.Normalize();
+	public Quaternion GetRotationUpdate(float deltaTime, Transform baseTransform)
+	{
+		Vector3 dir = objective.transform.position - baseTransform.position;
+		dir.Normalize();
 
-        float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
-        float firstRotzAngle;
-        if (zAngle > 0)
-        {
-            firstRotzAngle = zAngle;
-        } else
-        {
-            firstRotzAngle = 360 + zAngle;
-        }
-        var remainingRot = Math.Abs(baseTransform.rotation.eulerAngles.z) - Math.Abs(firstRotzAngle); 
+		float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+		float firstRotzAngle;
+		if (zAngle > 0)
+		{
+			firstRotzAngle = zAngle;
+		}
+		else
+		{
+			firstRotzAngle = 360 + zAngle;
+		}
+		var remainingRot = Math.Abs(baseTransform.rotation.eulerAngles.z) - Math.Abs(firstRotzAngle);
 
-        if (remainingRot > 45)
-        {
-            speed = mediumRotationSpeed;
-        } else if (remainingRot > 15){
-            speed = slowRotationSpeed;
-        } else
-        {
-            speed = mediumRotationSpeed;
-        }
+		if (remainingRot > 45)
+		{
+			speed = mediumRotationSpeed;
+		}
+		else if (remainingRot > 15)
+		{
+			speed = slowRotationSpeed;
+		}
+		else
+		{
+			speed = mediumRotationSpeed;
+		}
 
-        Quaternion desiredRotation = Quaternion.Euler(0, 0, zAngle);
-        return Quaternion.RotateTowards(baseTransform.rotation, desiredRotation, (speed * deltaTime));
+		Quaternion desiredRotation = Quaternion.Euler(0, 0, zAngle);
+		return Quaternion.RotateTowards(baseTransform.rotation, desiredRotation, (speed * deltaTime));
 
-    }
+	}
 }
 
 public class InstantaneousRotationer : IRotationer
 {
-    public GameObject objective;
+	public GameObject objective;
 
-    public InstantaneousRotationer(GameObject objective)
-    {
-        this.objective = objective;
+	public InstantaneousRotationer(GameObject objective)
+	{
+		this.objective = objective;
 
-    }
+	}
 
-    public Quaternion GetRotationUpdate(float deltaTime, Transform baseTransform)
-    {
-       
-        Vector3 dir = objective.transform.position - baseTransform.position;
-        dir.Normalize();
+	public Quaternion GetRotationUpdate(float deltaTime, Transform baseTransform)
+	{
 
-        float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
+		Vector3 dir = objective.transform.position - baseTransform.position;
+		dir.Normalize();
 
-        Quaternion desiredRotation = Quaternion.Euler(0, 0, zAngle);
+		float zAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90;
 
-        return Quaternion.RotateTowards(baseTransform.rotation, desiredRotation, 360);
+		Quaternion desiredRotation = Quaternion.Euler(0, 0, zAngle);
 
-    }
+		return Quaternion.RotateTowards(baseTransform.rotation, desiredRotation, 360);
+
+	}
 }
 
 
 public class FaceTaggedObject : MonoBehaviour
 {
 
-    private GameObject objective;
+	private GameObject objective;
 
-    public IRotationer rotationer { get; private set; }
+	public IRotationer rotationer { get; private set; }
 
-    public string tagToFace;
-    public bool instantaneous;
+	public string tagToFace;
+	public bool instantaneous;
 
-    // Start is called before the first frame update
-    void Awake()
-    {
-        FindObjective();
+	// Start is called before the first frame update
+	void Awake()
+	{
+		FindObjective();
 
-    }
+	}
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (objective == null)
-        {
-            FindObjective();
-        }
+	// Update is called once per frame
+	void FixedUpdate()
+	{
+		if (objective == null)
+		{
+			FindObjective();
+		}
 
-        if (objective == null)
-            return;
+		if (objective == null)
+			return;
 
-        transform.rotation = rotationer.GetRotationUpdate(Time.deltaTime, transform);
+		transform.rotation = rotationer.GetRotationUpdate(Time.deltaTime, transform);
 
-    }
+	}
 
-    private void FindObjective()
-    {
-        if (String.IsNullOrEmpty(tagToFace))
-        {
-            objective = null;
-        }
-        else
-        {
-            //Find the desired target
-            GameObject go = GameObject.FindWithTag(tagToFace);
+	private void FindObjective()
+	{
+		if (String.IsNullOrEmpty(tagToFace))
+		{
+			objective = null;
+		}
+		else
+		{
+			//Find the desired target
+			GameObject go = GameObject.FindWithTag(tagToFace);
 
-            if (go != null)
-            {
-                objective = go;
-                if (instantaneous)
-                {
-                    rotationer = new InstantaneousRotationer(go);
-                } else
-                {
-                    rotationer = new DampRotationer(go, this.GetComponent<SpriteRenderer>());
-                }
-                
-            }
-        }
+			if (go != null)
+			{
+				objective = go;
+				if (instantaneous)
+				{
+					rotationer = new InstantaneousRotationer(go);
+				}
+				else
+				{
+					rotationer = new DampRotationer(go, this.GetComponent<SpriteRenderer>());
+				}
 
-    }
+			}
+		}
+
+	}
 }
