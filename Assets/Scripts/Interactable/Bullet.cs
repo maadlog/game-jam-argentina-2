d8ad6dc8607c0c4ced724f794b0c65e7ba90f883
Romibarbosa <romy_jul@hotmail.com>
2019-09-27 13:39:19 -0300

    Boss's bullets are destructible

--- Assets/Scripts/Interactable/Bullet.cs
+++ Assets/Scripts/Interactable/Bullet.cs
@@ -16,4 +16,13 @@ public class Bullet : MonoBehaviour
     {
         transform.position += transform.right * bulletSpeed * Time.deltaTime;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Destroy(gameObject);
+
+        }
+    }
 }
